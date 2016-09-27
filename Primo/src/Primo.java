9492049804e0e5f35aa45e1a9e70de0f9b85bf17
rafaetur <rafaetur@gmail.com>
2016-09-27 20:40:57 -0300

    alterando nome da classe

--- Primo/src/Primo.java
+++ Primo/src/Primo.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Primo {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
